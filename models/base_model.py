717d2959f3a5a6281de149e70b68cdce81361a79
Donald <donumeh54@gmail.com>
2024-07-12 09:34:12 +0100

    updated the do_create function in console.py

--- models/base_model.py
+++ models/base_model.py
@@ -19,6 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            self.__dict__.update(kwargs)
             storage.new(self)
         else:
             kwargs["updated_at"] = datetime.strptime(
