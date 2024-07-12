8e655b0ac0f7360527e83b8fcdfc438c5f22d5df
Donald <donumeh54@gmail.com>
2024-07-12 10:36:25 +0100

    a script that sets up mysql server for the project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -9,10 +9,9 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 
 -- Grant `hbnb_dev` privileges in the database
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+
 
 -- Grant SELECT privilege on database `performance_schema`
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
 
